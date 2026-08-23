29fd760a1af5026537e601965ee94d4226658581
Kiwi Singh <metal.garuruwind.tetusaiga223@gmail.com>
2026-08-23 12:16:56 +0530

    Add files via upload

---
+++ Day 27 (Miles to Kilometers Converter Project)/main.py
@@ -0,0 +1,26 @@
+from tkinter import *
+window = Tk()
+window.title("Miles to kilometers converter")
+window.config(padx=20, pady=20)
+window.minsize(300, 200)
+miles_entry = Entry(window, width=7)
+miles_entry.insert(0,"0")
+miles_entry.grid(row=0, column=1)
+miles_text = Label(window, text="miles")
+miles_text.grid(row=0, column=2)
+is_equal_to_text = Label(window, text="is equal to")
+is_equal_to_text.grid(row=1, column=0)
+km_conv = Label(window, text="0")
+km_conv.grid(row=1, column=1)
+km_text = Label(window, text="km")
+km_text.grid(row=1, column=2)
+
+def convert_to_kilometers():
+    miles = miles_entry.get()
+    kilometres_unround = int(miles) * 1.609344
+    kilometres = round(kilometres_unround, 2)
+    km_conv.config(text=kilometres)
+
+convert_button = Button(window, text="Calculate", command=convert_to_kilometers)
+convert_button.grid(row=2, column=1)
+window.mainloop()
