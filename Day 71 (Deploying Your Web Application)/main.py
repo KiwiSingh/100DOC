7c86bd4518bb2fd5f627a8093c903f3c127c8287
Kiwi Singh <kiwisingh@proton.me>
2026-09-13 18:04:39 +0530

    Modify environment variables to match Neon

--- Day 71 (Deploying Your Web Application)/main.py
+++ Day 71 (Deploying Your Web Application)/main.py
@@ -28,12 +28,11 @@ Bootstrap5(app)
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-db_uri = os.environ.get("DB_URI", "sqlite:///posts.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
+    "DATABASE_URL",
+    "sqlite:///posts.db"
+)
 
-if db_uri.startswith("postgres://"):
-    db_uri = db_uri.replace("postgres://", "postgresql://", 1)
-
-app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 login_manager=LoginManager()
